2f643eaaa32f9262bd7cd9e35891e9398a0c47da
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-02 12:12:45 +0200

    setting up card app

---
+++ MpiloCard/MpiloCard/MpiloCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MpiloCardApp.swift
+//  MpiloCard
+//
+//  Created by Mpilo Pillz on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MpiloCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
