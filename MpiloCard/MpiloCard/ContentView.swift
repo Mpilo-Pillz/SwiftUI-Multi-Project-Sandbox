84edb9b809cccf096a3078a4f39a7fd0337c8c20
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-04 19:30:45 +0200

    adding phone number details

--- MpiloCard/MpiloCard/ContentView.swift
+++ MpiloCard/MpiloCard/ContentView.swift
@@ -12,10 +12,22 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00).ignoresSafeArea(edges: .all)
             VStack {
-                Image("beerPillz").resizable().aspectRatio(contentMode: .fit).frame(width: 250, height: 250).clipShape(Circle())
+                Image("beerPillz").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150).clipShape(Circle())
                 
                 Text("Mpilo Pillz").font(Font.custom("Pacifico-Regular", size: 40)).bold().foregroundStyle(.white)
                 Text("iOS Developer").foregroundStyle(.white).font(.system(size: 25))
+                Divider()
+                RoundedRectangle(cornerRadius: 25)
+                   .fill(Color.white)
+                   .frame(height: 50)
+                   .foregroundColor(.white)
+                   .overlay {
+                       HStack {
+                           Image(systemName: "phone.fill").foregroundStyle(Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00))
+                           Text("+2726274261")
+                       }
+                       
+                   }
             }
         }
     }
