1b4df66d026960c711e5da5bd5a1890155d9be1f
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-06 20:01:54 +0200

    creating dice game

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mpilo Pillz on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
