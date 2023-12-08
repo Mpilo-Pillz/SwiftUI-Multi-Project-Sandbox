a6456747affe81e476b625f07534aa65e86c51db
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-08 21:01:00 +0200

    fixing button touching the bottom

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -15,21 +15,31 @@ struct ContentView: View {
             
             VStack {
                 Image("diceeLogo")
+                Spacer()
                 HStack {
                     DiceView(n: 1)
                     DiceView(n: 2)
                 }
+                .padding(.horizontal)
+                Spacer()
                 Button(action: {
-                    print("clicked")
-                }, label: {
+                    // Your action here
+                    print("Roll button clicked")
+                }) {
                     Text("Roll")
-                        .font(.system(size: 50))
+                        .font(.system(size: 40)) // Adjust the font size if needed
                         .fontWeight(.heavy)
-                        .foregroundStyle(.white)
-                })
-                .background(Color.red)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 250, height: 60) // Adjust the frame if needed
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+//                        .cornerRadius(30) // This will round the corners
+                        .shadow(radius: 10) // This adds a shadow
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 20)
             }
-            .padding(.horizontal)
+           
             
         }
     }
@@ -45,5 +55,6 @@ struct DiceView: View {
         Image("dice\(n)")
             .resizable()
             .aspectRatio(contentMode: .fit)
+            .padding()
     }
 }
