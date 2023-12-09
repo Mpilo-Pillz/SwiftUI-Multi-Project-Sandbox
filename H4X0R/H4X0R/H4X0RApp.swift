6e3e59c3dca07a280d5e0a13fdbe162d8f09d96b
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-09 19:48:36 +0200

    creating hacker news app

---
+++ H4X0R/H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Mpilo Pillz on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
