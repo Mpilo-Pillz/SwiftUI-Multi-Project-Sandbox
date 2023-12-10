2e60fd92633ba3a00003c01c607f4787b9c783db
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-10 20:15:42 +0200

    addig post data type

---
+++ H4X0R/H4X0R/Models/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  H4X0R
+//
+//  Created by Mpilo Pillz on 2023/12/23.
+//
+
+import Foundation
---
+++ H4X0R/H4X0R/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R
+//
+//  Created by Mpilo Pillz on 2023/12/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
