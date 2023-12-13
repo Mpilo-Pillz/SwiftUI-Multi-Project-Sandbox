680f1217bc0ce66720d4aa0f8043ecffd4ee2183
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-13 23:29:36 +0200

    creting detail view

---
+++ H4X0R/H4X0R/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  H4X0R
+//
+//  Created by Mpilo Pillz on 2023/12/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DetailView(url: "https://www.google.com")
+}
