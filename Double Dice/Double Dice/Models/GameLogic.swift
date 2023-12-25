98e0b8b92604fbcf9cd47f96ac567040f965e73a
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-25 02:47:00 +0200

    adding game logic

---
+++ Double Dice/Double Dice/Models/GameLogic.swift
@@ -0,0 +1,29 @@
+//
+//  GameLogic.swift
+//  Double Dice
+//
+//  Created by Mpilo Pillz on 2023/12/28.
+//
+
+import Foundation
+
+struct GameLogic {
+    
+    var numberOfDiceRollsLeft: Int = 10
+    var totalPoints: Int = 0
+    
+    private func isDoube(diceOne: Int, diceTwo: Int) -> Bool {
+        return diceOne == diceTwo
+    }
+    
+    mutating func checkIfDouble(diceOne: Int, diceTwo: Int) {
+        
+        if isDoube(diceOne: diceOne, diceTwo: diceTwo) {
+            totalPoints = totalPoints + diceOne + diceTwo
+            numberOfDiceRollsLeft += 1
+        } else {
+            numberOfDiceRollsLeft -= 1
+        }
+    }
+    
+}
