1bcc77e904d59168cdc9aa8c5ba9357c9e201528
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-27 02:47:00 +0200

    inverting control of the instance of game logic to the app

--- Double Dice/Double Dice/Double_DiceApp.swift
+++ Double Dice/Double Dice/Double_DiceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Double_DiceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(gameLogic: GameLogic())
         }
     }
 }
