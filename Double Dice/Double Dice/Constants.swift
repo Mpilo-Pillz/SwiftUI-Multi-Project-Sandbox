f0fbbdc8f0579eb8fc5a39d1627ae7af1591a3ed
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-12-31 19:32:39 +0200

    moving image strings names to contsnts file

---
+++ Double Dice/Double Dice/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Double Dice
+//
+//  Created by Mpilo Pillz on 2023/12/31.
+//
+
+import Foundation
+
+struct K {
+    static let backgroundImage = "background"
+    static let doubleDiceLogo = "diceeLogo"
+}
