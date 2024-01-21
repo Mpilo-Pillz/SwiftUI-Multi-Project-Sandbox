338b082799085708f393b50e399c110a7257b013
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-21 20:45:52 +0200

    extracting secure textfirld to reusable text field

--- Tsanyela/Tsanyela/Views/Controls/TextInputView.swift
+++ Tsanyela/Tsanyela/Views/Controls/TextInputView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct TextInputView: View {
-let placeholder: String
-    let textValue: Binding<String>
+    let placeholder: String
+//    let textValue: Binding<String> if I ever want to perform additional manipulations
+    @Binding var textValue: String
+    
+    var isSecure = false
+    
     var body: some View {
-        TextField(placeholder, text: textValue)
-            .padding()
-//                .textFieldStyle(RoundedBorderTextFieldStyle())
-            .background(Color(.systemGray6)).cornerRadius(5.0)
+        Group {
+            if isSecure {
+                SecureField(placeholder, text: $textValue)
+            } else {
+                TextField(placeholder, text: $textValue)
+            }
+        }
+        
+        .padding()
+        //                .textFieldStyle(RoundedBorderTextFieldStyle())
+        .background(Color(.systemGray6)).cornerRadius(5.0)
     }
 }
 
