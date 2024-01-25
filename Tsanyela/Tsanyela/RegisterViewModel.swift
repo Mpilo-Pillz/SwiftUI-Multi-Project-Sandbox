9b176b28d563946e354bc712e7bad109bd0434b5
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-25 22:34:52 +0200

    creating reg viiew model

--- Tsanyela/Tsanyela/RegisterViewModel.swift
+++ Tsanyela/Tsanyela/RegisterViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+class RegisterViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var isLoggedIn: Bool = false
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+
+    func login() async {
+        isLoading = true
+        errorMessage = nil
+        
+        do {
+            let user = try await loginUser(username: username, password: password)
+            isLoggedIn = true
+        } catch {
+            
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+    
+    private func loginUser(username: String, password: String) async throws -> User {
+        let dummyUser = User(id: "123", name: "John Doe")
+        return dummyUser
+    }
+}
