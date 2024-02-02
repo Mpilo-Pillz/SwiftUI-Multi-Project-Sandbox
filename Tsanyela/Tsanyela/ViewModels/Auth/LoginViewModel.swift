26c737580e94fe288b42f27f9239735a5cc3f395
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-02-02 22:19:08 +0200

    successfully making call and geting access token

--- Tsanyela/Tsanyela/ViewModels/Auth/LoginViewModel.swift
+++ Tsanyela/Tsanyela/ViewModels/Auth/LoginViewModel.swift
@@ -15,12 +15,19 @@ class LoginViewModel: ObservableObject {
     @Published var errorMessage: String?
     
     func login() async {
+        guard !email.isEmpty, !password.isEmpty else {
+            return
+        }
+        
+        isLoading = true
+        defer { isLoading = false }
+        
         let loginURL = URL(string: "http://localhost:3000/tsanyela/auth/login")!
         var request = URLRequest(url: loginURL)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        let body = ["email": email, "password": password]
+        let body: [String: String] = ["email": email, "password": password]
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
         
         isLoading = true
@@ -29,22 +36,19 @@ class LoginViewModel: ObservableObject {
             //            let user = try await loginUser(username: email, password: password)
             let (data, response) = try await URLSession.shared.data(for: request)
             
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                DispatchQueue.main.async {
-                    self.errorMessage = "Invalid response from server"
-                    self.isLoading = false
-                }
+            if let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data)  {
+                print("Access Token: \(loginResponse.access_token)")
+                UserDefaults.standard.set(loginResponse.access_token, forKey: "access_token")
                 return
-            }
-            DispatchQueue.main.async {
-                self.isLoggedIn = true
+            } else {
+                print("decoding failed")
             }
             
         } catch {
-            
+            print("Login request failed with error: \(error)")
             DispatchQueue.main.async {
                 self.errorMessage = "Login failed: \(error.localizedDescription)"
-                self.isLoading = false
+//                self.isLoading = false
             }
         }
         
@@ -54,4 +58,8 @@ class LoginViewModel: ObservableObject {
         let dummyUser = User(id: "123", name: "John Doe")
         return dummyUser
     }
+}
+
+struct LoginResponse: Codable {
+    let access_token: String
 }
